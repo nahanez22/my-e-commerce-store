13d1811c2de38250317d344b49fea64bc9e9b5ba
Nahum Añez <nahumatencio@gmail.com>
2023-01-03 23:52:44 -0500

    Creating context

---
+++ src/CartContext.jsx
@@ -0,0 +1,36 @@
+import { createContext } from "react";
+
+// Inicializar el contexto
+const CartContext = createContext();
+
+// Proporcionar el Provider
+const CartProvider = ({ children }) => {
+  // Estado del carrito de compras
+  const [cart, setCart] = useState([]);
+
+  // Método para agregar un regalo al carrito de compras
+  const handleAdd = (gift, quantity) => {
+    const newGift = { ...gift, cantidad: quantity }; // Crear una copia del regalo y agregar la cantidad seleccionada
+    setCart([...cart, newGift]); // Agregar el regalo al carrito de compras
+  };
+
+  // Método para eliminar un regalo del carrito de compras
+  const handleRemove = (id) => {
+    setCart(cart.filter((gift) => gift.id !== id));
+  };
+
+  return (
+    <CartContext.Provider
+      value={{
+        cart,
+        setCart,
+        handleAdd,
+        handleRemove,
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+export { CartContext, CartProvider };
